bf71e6a02827c5f217cf3578a20330d619d47132
TorresiLeonardo <torresi.leo03@gmail.com>
2021-11-24 12:29:06 +0100

    gabbra toast

--- app/src/main/java/itts/quintodinf/lavoretto/MainActivity.java
+++ app/src/main/java/itts/quintodinf/lavoretto/MainActivity.java
@@ -10,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent i i2=getIntent();
+        String messaggiricevuti = i.getStringExtra(name “messaggio”);
+        String nomiricevuto = i2.getStringExtra(name “nome”);
+        Toast t= Toast.makeText(getApplicationContext(), nomericevuto.toString(), Toast.LENGHT_LONG);
+        t.show();
+        //t.setGravity(10, 3, 5);
+    }
+}
+
     }
 }
